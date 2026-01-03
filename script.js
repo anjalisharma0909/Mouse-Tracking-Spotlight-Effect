e64a00962d940d66bbc203e1be4f1f799c322968
Anjali Sharma <bhpndrdhrm@gmail.com>
2026-01-03 05:46:25 -0800

    Add mouse tracking spotlight effect
     Interactive spotlight using CSS and JavaScript.

---
+++ script.js
@@ -0,0 +1,5 @@
+addEventListener("mousemove", (e) => {
+    console.log(e.clientX, e.clientY)
+    document.body.style.setProperty("--X", e.clientX + 'px')
+    document.body.style.setProperty("--Y", e.clientY + "px")
+})
